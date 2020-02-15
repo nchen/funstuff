d7ac621b4af02b22d0313a72a323d930d64d1c4b
woodfox <woodfox@wfmbp.local>
2020-02-15 20:27:07 +0800

    Fix the build root path for game of life

---
+++ game-of-life/precache-manifest.b4b677b884d1a57aa0f801ca5850e8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b9e4d9dd646bd5a9ffbae362e3bb35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b755b5d4ac1af2c00d3",
+    "url": "./static/css/main.ec5d1c17.chunk.css"
+  },
+  {
+    "revision": "fe0080073baa10c47baa",
+    "url": "./static/js/2.90e93839.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.90e93839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b755b5d4ac1af2c00d3",
+    "url": "./static/js/main.16d3d45f.chunk.js"
+  },
+  {
+    "revision": "3a2195381b63c16a8f57",
+    "url": "./static/js/runtime-main.b29f7425.js"
+  }
+]);
